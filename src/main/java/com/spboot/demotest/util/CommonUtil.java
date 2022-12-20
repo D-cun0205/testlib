54b2488830ffaae465387bfdcafaf534db92d638
D-cun0205 <mmeo0205@gmail.com>
2022-12-20 15:51:20 +0900

    spring actuator

--- src/main/java/com/spboot/demotest/util/CommonUtil.java
+++ src/main/java/com/spboot/demotest/util/CommonUtil.java
@@ -3,9 +3,6 @@ package com.spboot.demotest.util;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class CommonUtil {
@@ -13,19 +10,5 @@ public class CommonUtil {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
-    }
-
-    @Bean
-    public JavaMailSenderImpl javaMailSender() {
-        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.setProtocol("SMTP");
-        javaMailSender.setHost("localhost");
-        javaMailSender.setPort(25);
-
-        Properties properties = new Properties();
-        properties.setProperty("mail.smtp.auth", "false");
-        javaMailSender.setJavaMailProperties(properties);
-
-        return javaMailSender;
     }
 }
